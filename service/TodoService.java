ac3a433ee7418e564711207161dd5cc6bd87617c
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2023-06-20 22:46:01 +0900

    백엔드 개발 18. @NamedQuery 사용하기

--- service/TodoService.java
+++ service/TodoService.java
@@ -13,12 +13,12 @@ public class TodoService {
 	private TodoRepository repository;
 	
 	public String testService(){
-		// TodoEntity 생성
-		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
-		// TodoEntity 저장
+		// Todo Entity 생성
+		TodoEntity entity = TodoEntity.builder().userId("Lee Ji Min").title("My first todo item").build();
+		// Todo Entity 저장
 		repository.save(entity);
-		// TodoEntity 검색
-		TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
+		// Todo Entity 검색
+		TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
 		return savedEntity.getUserId();
 	}
 }
